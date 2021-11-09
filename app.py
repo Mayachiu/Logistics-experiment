a79e4f4268ff2b96c8223c66151635c6f5511776
Mayachiu <mayachiu5g@gmail.com>
2021-11-10 07:20:21 +0900

    commit

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
-st.title("タイトル")
+st.title("流通情報工学実験データ分析アプリ")
+st.subheader("by Kazuki Uchiyama")
+
+uploaded_file = st.file_uploader("ファイルを選んでください。", type="csv")
+
+data = pd.read_csv(uploaded_file, encoding="shift-jis")
+data.head()
