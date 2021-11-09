cd5f9606ddb314aedd1145c066cb3bc693a9a518
Mayachiu <mayachiu5g@gmail.com>
2021-11-10 07:27:23 +0900

    commit

--- app.py
+++ app.py
@@ -9,4 +9,20 @@ st.subheader("by Kazuki Uchiyama")
 uploaded_file = st.file_uploader("ファイルを選んでください。", type="csv")
 
 data = pd.read_csv(uploaded_file, encoding="shift-jis")
-st.dataframe(data, width=1500)
+st.dataframe(data)
+
+#select_list=["時刻","緯度","経度","X軸加速度","Y軸加速度","Z軸加速度","方位","ピッチ","ロール",""]
+select_list= list(data)
+option = st.sidebar.selectbox(
+    "x軸に表示するデータを選んでください",
+    select_list
+)
+
+"x軸は",option,"です"
+
+option2 = st.sidebar.selectbox(
+    "y軸に表示するデータを選んでください",
+    select_list
+)
+
+"y軸は",option2,"です"
