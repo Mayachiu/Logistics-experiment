3ba49201694dcc4ba0ad637e27be8f8a31280500
Mayachiu <mayachiu5g@gmail.com>
2021-11-10 08:54:37 +0900

    commit

--- app.py
+++ app.py
@@ -8,27 +8,28 @@ st.subheader("by Kazuki Uchiyama")
 
 uploaded_file = st.file_uploader("ファイルを選んでください。", type="csv")
 
-data = pd.read_csv(uploaded_file, encoding="shift-jis")
-st.dataframe(data)
+if uploaded_file is not None:
+    data = pd.read_csv(uploaded_file, encoding="shift-jis")
+    st.dataframe(data)
 
-#列名取得
-select_list= list(data)
+    #列名取得
+    select_list= list(data)
 
-option1 = st.sidebar.selectbox(
-    "x軸に表示するデータを選んでください",
-    select_list
-)
+    option1 = st.sidebar.selectbox(
+        "x軸に表示するデータを選んでください",
+        select_list
+    )
 
-"x軸は",option1,"です"
+    "x軸は",option1,"です"
 
-option2 = st.sidebar.selectbox(
-    "y軸に表示するデータを選んでください",
-    select_list
-)
+    option2 = st.sidebar.selectbox(
+        "y軸に表示するデータを選んでください",
+        select_list
+    )
 
-"y軸は",option2,"です"
+    "y軸は",option2,"です"
 
 
-st.write(px.scatter(data,x =option1 ,y = option2))
+    st.write(px.scatter(data,x =option1 ,y = option2))
 
-st.write(px.histogram(data,x =option1 ,y = option2))
+    st.write(px.histogram(data,x =option1 ,y = option2))
