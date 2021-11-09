6cc314eba24f11a6d2a6a0195cb476eeee57940b
Mayachiu <mayachiu5g@gmail.com>
2021-11-10 07:38:12 +0900

    commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 
-st.title("流通情報工学実験データ分析アプリ")
+st.title("流通情報工学実験データ分析アプリ(β)")
 st.subheader("by Kazuki Uchiyama")
 
 uploaded_file = st.file_uploader("ファイルを選んでください。", type="csv")
@@ -14,12 +14,12 @@ st.dataframe(data)
 #列名取得
 select_list= list(data)
 
-option = st.sidebar.selectbox(
+option1 = st.sidebar.selectbox(
     "x軸に表示するデータを選んでください",
     select_list
 )
 
-"x軸は",option,"です"
+"x軸は",option1,"です"
 
 option2 = st.sidebar.selectbox(
     "y軸に表示するデータを選んでください",
@@ -28,4 +28,7 @@ option2 = st.sidebar.selectbox(
 
 "y軸は",option2,"です"
 
-st.write(px.scatter(data,x =option,y = option2))
+
+st.write(px.scatter(data,x =option1 ,y = option2))
+
+st.write(px.histogram(data,x =option1 ,y = option2))
