1caa664bb24111b6f0a69565ef0542f2860c98e3
Mayachiu <mayachiu5g@gmail.com>
2021-11-10 09:05:04 +0900

    commit

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import plotly.express as px
 st.title("流通情報工学実験データ分析アプリ(β)")
 st.subheader("by Kazuki Uchiyama")
 
+st.write("時刻変換後のCSVファイル(XXX_t.csv)をアップロード")
 uploaded_file = st.file_uploader("ファイルを選んでください。", type="csv")
 
 if uploaded_file is not None:
@@ -32,8 +33,8 @@ if uploaded_file is not None:
 
     st.write(px.scatter(data,x =option1 ,y = option2))
 
-    st.write(px.histogram(data,x =option1 ,y = option2))
-
     st.write(px.line(data,x =option1 ,y = option2))
+
+    st.write(px.histogram(data,x =option1 ,y = option2))
 
     st.write(px.scatter_3d(data, x="X軸加速度", y="Y軸加速度", z="Z軸加速度"))
